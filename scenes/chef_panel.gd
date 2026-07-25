fc72b459d387656d1e359be1632a2884d0a91106
David Sulpice <128552915+Kurooco@users.noreply.github.com>
2026-07-24 22:08:06 -0400

    added chef panel

---
+++ scenes/chef_panel.gd
@@ -0,0 +1,16 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameManager.player_reset.connect(set_expression.bind(0))
+	get_tree().get_first_node_in_group("count_tracker").satisfied.connect(set_expression.bind(1))
+	get_tree().get_first_node_in_group("count_tracker").dissatisfied.connect(set_expression.bind(2))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func set_expression(frame:int):
+	$Sprite2D.frame = frame
