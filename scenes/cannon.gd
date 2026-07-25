29db768d47aaab91795a9cb15183a5774e107e7e
David Sulpice <128552915+Kurooco@users.noreply.github.com>
2026-07-24 20:45:36 -0400

    added cannon trails

--- scenes/cannon.gd
+++ scenes/cannon.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var force = 500
 @onready var shoot_particles: CPUParticles2D = $Sprite2D/CPUParticles2D
 var player : Player = null
 var size_tween : Tween
@@ -7,11 +8,12 @@ var normal_size : Vector2
 
 func _ready() -> void:
 	normal_size = $Sprite2D.scale
+	make_line()
 
 
 func _on_timer_timeout() -> void:
 	player.disable(false)
-	player.apply_impulse(Vector2.from_angle(rotation)*500)
+	player.apply_impulse(Vector2.from_angle(rotation)*force)
 	player.show()
 	shoot_particles.emitting = true
 
@@ -27,3 +29,14 @@ func _on_body_entered(body: Node2D) -> void:
 			size_tween.kill()
 		size_tween = create_tween()
 		size_tween.tween_property($Sprite2D, "scale", normal_size, .3).from(normal_size*2)
+
+func make_line():
+	$Line2D.global_rotation = 0
+	var curr = Vector2.ZERO
+	var vel = Vector2.from_angle(rotation)*force
+	print_debug(vel)
+	for i in range(800):
+		$Line2D.add_point(curr)
+		curr += vel*.001
+		vel.y += ProjectSettings.get_setting("physics/2d/default_gravity")*.001
+		print_debug("vel "+str(vel)+" curr "+str(curr))
