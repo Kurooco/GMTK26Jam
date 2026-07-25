06ddb40fb778370ce4c41128416e8d955313076e
David Sulpice <128552915+Kurooco@users.noreply.github.com>
2026-07-25 13:44:40 -0400

    added transitioning music

--- scripts/level_handler.gd
+++ scripts/level_handler.gd
@@ -1,11 +1,20 @@
 extends Node
 
 @export var levels : Array[PackedScene]
+@export var music_transition_levels : Array[int]
 @onready var fade_screen: ColorRect = $CanvasLayer/Fade
 var current_level_ind = -1
 var current_level = null
 
 var fade_tween : Tween
+var music_order = [
+	preload("res://sounds/music/Only You Could Know - v1.wav"),
+	preload("res://sounds/music/Only You Could Know - v2.wav"),
+	preload("res://sounds/music/Only You Could Know - v3.wav"),
+	preload("res://sounds/music/Only You Could Know - v4.wav"),
+	preload("res://sounds/music/Only You Could Know - v5.wav")
+]
+var current_song = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,6 +36,7 @@ func next_level():
 	await fade_tween.finished
 	GameManager.current_mode = GameManager.PlayMode.PLACING
 	set_level(current_level_ind+1)
+	check_level_music(current_level_ind)
 	fade(false)
 
 func fade(out:bool):
@@ -39,4 +49,13 @@ func fade(out:bool):
 		fade_tween.tween_property(fade_screen, "position:x", -200, 1.5).set_ease(Tween.EASE_OUT).from(1200)
 	else:
 		fade_tween.tween_property(fade_screen, "position:x", -2000, 1).set_ease(Tween.EASE_IN)
-	
+
+
+func check_level_music(ind:int):
+	if(music_transition_levels[current_song] <= ind):
+		current_song += 1
+		print_debug(current_song)
+		var pos = $Music.get_playback_position()
+		print_debug(pos)
+		$Music.stream = music_order[current_song]
+		$Music.play(pos)
